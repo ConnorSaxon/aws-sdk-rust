886dbe16c363aee15d0ec2ebd356de04cf2ce74b
ConnorSaxon <123480219+ConnorSaxon@users.noreply.github.com>
2024-04-17 21:25:27 -0400

    Update v4a.rs to comment out un-supported methods
    comment out lines 68-74 and 78

--- sdk/aws-sigv4/src/sign/v4a.rs
+++ sdk/aws-sigv4/src/sign/v4a.rs
@@ -65,17 +65,17 @@ pub fn generate_signing_key(access_key: &str, secret_access_key: &str) -> impl A
 
         // It would be more secure for this to be a constant time comparison, but because this
         // is for client usage, that's not as big a deal.
-        if k0 <= *BIG_N_MINUS_2 {
-            let pk = k0
-                .checked_add(&U256::ONE)
-                .expect("k0 is always less than U256::MAX");
-            let d = Zeroizing::new(pk.to_be_bytes());
-            break SigningKey::from_bytes(d.as_ref()).unwrap();
-        }
+        // if k0 <= *BIG_N_MINUS_2 {
+        //     let pk = k0
+        //         .checked_add(&U256::ONE)
+        //         .expect("k0 is always less than U256::MAX");
+        //     let d = Zeroizing::new(pk.to_be_bytes());
+        //     break SigningKey::from_bytes(d.as_ref()).unwrap();
+        // }
 
         *counter = counter
             .checked_add(1)
-            .expect("counter will never get to 255");
+            // .expect("counter will never get to 255");
     };
 
     key.to_bytes()
